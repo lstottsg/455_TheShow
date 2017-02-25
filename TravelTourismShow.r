05c02ec2a3171fe4fcbfdd9e54a5c949d5bc4dec
mjungebl <mexicali@gmail.com>
2017-02-25 13:39:14 -0500

    Modified to reflect current directory structure
    Modified to reflect current directory structure

--- TravelTourismShow.r
+++ TravelTourismShow.r
@@ -35,31 +35,31 @@ library(tidyr)
 
 
 # cities focused on in HBR article
-Lisbon <- read.csv("Lisbon.csv")
-JoshuaTree <- read.csv("JoshuaTree.csv")
-Reykjavik <- read.csv("Reykjavik.csv")
+#Lisbon <- read.csv("Lisbon.csv")
+#JoshuaTree <- read.csv("JoshuaTree.csv")
+#Reykjavik <- read.csv("Reykjavik.csv")
 
 # supplementary cities mentioned in HBR article
-Paris_L <- read.csv("Paris Listings.csv")
-Paris_R <- read.csv("Paris Reviews.csv")
-NewYork_L <- read.csv("New York Listings.csv")
-NewYork_R <- read.csv("New York Reviews.csv")
-Barcelona_L <- read.csv("Barcelona Listings.csv")
-Barcelona_R <- read.csv("Barcelona Reviews.csv")
+Paris_L <- read.csv("./cities/paris/listings.csv")
+Paris_R <- read.csv("./cities/paris/reviews.csv")
+NewYork_L <- read.csv("./cities/new york/listings.csv")
+NewYork_R <- read.csv("./cities/new york/reviews.csv")
+Barcelona_L <- read.csv("./cities/barcelona/listings.csv")
+Barcelona_R <- read.csv("./cities/barcelona/reviews.csv")
 
 #detailed reviews for text analysis
-ParisDetRev <- read.csv("Paris Detailed Reviews.csv.gz")
-NewYorkDetRev <- read.csv("New York Detailed Reviews.csv.gz")
-BarcelonaDetRev <- read.csv("Barcelona Detailed Reviews.csv.gz")
+#ParisDetRev <- read.csv("Paris Detailed Reviews.csv.gz")
+#NewYorkDetRev <- read.csv("New York Detailed Reviews.csv.gz")
+#BarcelonaDetRev <- read.csv("Barcelona Detailed Reviews.csv.gz")
 
 ######################################################################################################################
 #Begin EDA
 ######################################################################################################################
 
 #set up eda data sets to play with
-Lisbon.eda <- Lisbon
-JoshuaTree.eda <- JoshuaTree
-Reykjavik.eda <- Reykjavik
+#Lisbon.eda <- Lisbon
+#JoshuaTree.eda <- JoshuaTree
+#Reykjavik.eda <- Reykjavik
 
 Paris_L.eda <- Paris_L
 Paris_R.eda <- Paris_R
@@ -73,9 +73,9 @@ NewYorkDetRev.eda <- NewYorkDetRev
 BarcelonaDetRev.eda <- BarcelonaDetRev
   
 #Check out summary for overview and identify missing
-summary(Lisbon.eda)
-summary(JoshuaTree.eda)
-summary(Reykjavik.eda)
+#summary(Lisbon.eda)
+#summary(JoshuaTree.eda)
+#summary(Reykjavik.eda)
 summary(Paris_L.eda)
 summary(Paris_R.eda)
 summary(NewYork_L.eda)
