016430153e15ccac28eff342311a422473485882
mjungebl <mexicali@gmail.com>
2017-03-11 22:31:22 -0500

    More changes per the comments

---
+++ R_code_bar_pie_charts_erin/propertynumber_owner.R
@@ -0,0 +1,47 @@
+property_counts <- read.csv("paris_listed3.csv", header=TRUE, sep=",", as.is=TRUE)
+
+pdf(file = "paris_barchart.pdf", width = 11, height = 8.5)
+
+ggplot(data=property_counts, aes(x=properties, y=owners)) +
+  geom_bar(colour="dark blue", fill="dark blue", width=0.8,stat="identity") +
+  xlab("Numbers of Properties Owned") + ylab("Numbers of Owners") +
+  ggtitle("The Total Numbers of AirBnB Rental Properties Owned in Paris")
+
+dev.off()
+
+svg(file = "paris_barchart.svg", width = 11, height = 8.5)
+
+ggplot(data=property_counts, aes(x=properties, y=owners)) +
+  geom_bar(colour="dark blue", fill="dark blue", width=0.8,stat="identity") +
+  xlab("Numbers of Properties Owned") + ylab("Numbers of Owners") +
+  ggtitle("The Total Numbers of AirBnB Rental Properties Owned in Paris")
+
+dev.off()
+
+png(file = "paris_barchart.png", width = 800, height = 600)
+
+ggplot(data=property_counts, aes(x=properties, y=owners)) +
+  geom_bar(colour="dark blue", fill="dark blue", width=0.8,stat="identity") +
+  xlab("Numbers of Properties Owned") + ylab("Numbers of Owners") +
+  ggtitle("The Total Numbers of AirBnB Rental Properties Owned in Paris")
+
+dev.off()
+
+
+
+slices <- c(2854, 524872) 
+lbls <- c("Owned Multiple Propertities", "Owned Single Property)
+pct <- round(slices/sum(slices)*100)
+lbls <- paste(lbls, pct)
+lbls <- paste(lbls,"%",sep="")
+pie(slices,labels = lbls, col=rainbow(length(lbls)),main="Owned Single Property Versus Multiple Properties | AirBnB in Paris")
+
+pdf(file = "/Volumes/WD/NU/predict_455/Group_assignments/CheckPointC/working_files/paris_piechart.pdf", width = 11, height = 8.5)
+
+slices <- c(2854, 524872) 
+lbls <- c("Owned Multiple Propertities", "Owned Single Property)
+pct <- round(slices/sum(slices)*100)
+lbls <- paste(lbls, pct)
+lbls <- paste(lbls,"%",sep="")
+pie(slices,labels = lbls, col=rainbow(length(lbls)),main="Owned Single Property Versus Multiple Properties | AirBnB in Paris")
+dev.off()
